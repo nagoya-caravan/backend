ddecbc9f8f49c835c513faa85d8540f6babd9630
kigawa <kigawa.8390@gmail.com>
2023-11-10 21:16:00 +0900

    init

--- app.py
+++ app.py
@@ -5,24 +5,22 @@ from flask import Flask
 from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
 
-
-def getenv_or_default(key: str, default: str) -> str:
-    result = os.getenv(key)
-    if result is None:
-        return default
-    return result
-
-
 dotenv.load_dotenv()
 app = Flask(__name__)
 
 db_user = os.getenv("DB_USER")
 db_pass = os.getenv("DB_PASS")
 db_host = os.getenv("DB_HOST")
+if db_host is None:
+    db_host = "localhost"
 db_port = os.getenv("DB_PORT")
+if db_port is not None:
+    db_host = f"{db_host}:{db_port}"
 db_name = os.getenv("DB_NAME")
+if db_name is None:
+    db_name = "backend"
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/sales-mail-assist'
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{db_user}:{db_pass}@{db_host}/{db_name}"
 app.config['JSON_AS_ASCII'] = False
 app.config["JWT_SECRET_KEY"] = os.getenv("LOGIN_SECRET")
 
--- readme.md
+++ readme.md
@@ -5,6 +5,22 @@
 
 ***
 
+## setup
+
+### pip install
+
+```shell
+pip install -r requirements.txt
+```
+
+### flask run
+
+```shell
+flask run
+```
+
+***
+
 ## framework/言語/ツール
 
 * python
